4f0c0358d24385cce7703683f7bfd3f63763a88f
ReshmaR29 <reshmareddy2211@gmail.com>
2025-07-18 16:16:05 +0530

    fix import error in permissions.py

--- courses/api/permissions.py
+++ courses/api/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermissionks
+from rest_framework.permissions import BasePermission
 
 class IsEnrolled(BasePermission):
     def has_object_permission(self, request, view, obj):
